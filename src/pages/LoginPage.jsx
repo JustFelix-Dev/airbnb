c476f58a6e46ab2a9a614a28c7a05aedd487df62
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-21 12:41:50 +0100

    airbnb

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -23,10 +23,10 @@ const LoginPage = () => {
     }
 
     const handleGoogle = ()=>{
-      window.open('https://www.airbnb.felixdev.com.ng/auth/google','_self')
+      window.open('https://www.airbnb-server.felixdev.com.ng/auth/google','_self')
   }
   const handleGithub = ()=>{
-      window.open('https://www.airbnb.felixdev.com.ng/auth/github','_self')
+      window.open('https://www.airbnb-server.felixdev.com.ng/auth/github','_self')
   }
    
     const handleForm=async(e)=>{
