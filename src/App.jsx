be719dbf29868e60a5e917a1403661afef0f3d60
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-25 17:08:28 +0100

    configured the app for PWA

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,21 @@ function App() {
   const [ user,setUser ] = useState(null)
 
   useEffect(()=>{
+    if('serviceWorker' in navigator){
+      window.addEventListener('load',()=>{
+        navigator.serviceWorker.register('/sw.js')
+        .then((registration)=>{
+           console.log('SW registered: ' , registration);
+        })
+        .catch(registrationError =>{
+          console.log('SW registration failed: ' , registrationError);
+        })
+      })
+
+    }
+  },[])
+
+  useEffect(()=>{
     async function fetchProfile(){
         try{
             if(!user){
@@ -42,7 +57,6 @@ function App() {
     fetchProfile();
   },[user])
 
- 
   
   return (
         <>
