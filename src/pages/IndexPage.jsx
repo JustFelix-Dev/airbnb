a075d0e6002b73f7195d40bf4c301949439b9995
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-26 15:23:44 +0100

    improved loading UI

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -15,7 +15,7 @@ const IndexPage = () => {
          <>
          <AnimatePresence>
           { loading && (
-           <motion.div exit={{opacity:0}} transition={{duration:1}} className='h-[80vh] w-full flex items-center justify-center bg-white'>
+           <motion.div exit={{opacity:0}} transition={{duration:2}} className='h-[80vh] w-full flex items-center justify-center bg-white'>
            <motion.div initial={{rotate:0,scale:0.8}} animate={{rotate:'360deg',scale:1}} transition={{duration:2, repeat:'infinity',repeatType:'loop'}}>
             <img src="/images/airbnb.png" alt="loader" height={100} width={100} />
            </motion.div>
@@ -24,10 +24,10 @@ const IndexPage = () => {
          </AnimatePresence>  
 
          { !loading &&
-          ( <motion.div  initial={{opacity:0}} animate={{opacity:1}} transition={{delay:3, when:"beforeChildren",staggerChildren:2}} className="mt-8 grid max-w-8xl pt-4 px-12 gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          ( <motion.div  initial={{opacity:0}} animate={{opacity:1}} transition={{delay:3}} className="mt-8 grid max-w-8xl pt-4 px-12 gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {
           allPlaces?.length > 0 && allPlaces.map((place,idx) => (
-            <motion.div initial={{scale:0.7,opacity:0.8}} animate={{scale:1,opacity:1}} transition={{duration:1}}>
+  
             <Link key={idx} to={'/place/'+place._id}>
           <div className="bg-gray-500 mb-2 rounded-2xl flex">
             {place.photos?.[0] && (
@@ -39,8 +39,7 @@ const IndexPage = () => {
           <div className="mt-1">
             <span className="font-bold">${place.price}</span> per night
           </div>
-            </Link>
-            </motion.div>
+        </Link>
       ))
       }
             </motion.div>)
