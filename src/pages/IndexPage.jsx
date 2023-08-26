a8dc55856e984d47896241425774db51d239dd6b
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-26 14:00:50 +0100

    improved loading UI

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -16,11 +16,11 @@ const IndexPage = () => {
            <div className="mt-8 grid max-w-8xl pt-4 px-12 gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {
           allPlaces?.length > 0 && allPlaces.map((place,idx) => (
-         <Link key={idx} to={'/place/'+place._id}>
+            loading ? <Skeleton/> : <Link key={idx} to={'/place/'+place._id}>
           <div className="bg-gray-500 mb-2 rounded-2xl flex">
             {place.photos?.[0] && (
               <img className="rounded-xl object-cover aspect-square " src={'https://www.airbnb-server.felixdev.com.ng/uploads/'+place.photos?.[0]} alt=""/> 
-            ) || <Skeleton/>}
+            )}
           </div>
           <h2 className="font-bold truncate">{place.address}</h2>
           <h3 className="text-sm text-gray-500">{place.title}</h3>
