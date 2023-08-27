e017ead07018ccac6526972b4b663b3d49044c0c
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-27 17:55:05 +0100

    airbnb

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -81,7 +81,7 @@ const Header = () => {
                       />
                 <div className='flex items-center gap-1 justify-center'>
                     <div className='pt-1 hidden md:block'>{ user && <div>{user.name.split(' ')[0]}</div>}</div>
-                 <div className='hidden md:block'>{ user && user.admin && <div><svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                 <div className='hidden md:block'>{ user && user.admin && <div><svg width={10} height={10} fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"></path>
                 </svg></div>}</div>   
                 </div>
