aa9d6e36fae7d02e42a84a4b8e3a84563df744a8
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-30 09:21:47 +0100

    airbnb

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -56,7 +56,7 @@ useEffect(()=>{
        e.preventDefault()
 
        if(!image){
-          toast.error('Photo is required!')
+          return toast.error('Photo is required!')
        }
        const formData = new FormData();
        formData.append('name', name);
@@ -128,7 +128,7 @@ useEffect(()=>{
                       </div>
                  </div>
                  <label className='my-2 bg-transparent cursor-pointer flex justify-center items-center gap-2 border border-gray-200 rounded-lg text-xl p-2'>
-                      <input type="file" className='hidden' accept='.jpeg,.jpg,.webP,.png' onChange={uploadPhoto} required/>
+                      <input type="file" className='hidden' accept='.jpeg,.jpg,.webP,.png' onChange={uploadPhoto} />
                       {
                       photo && photo.length > 0 ? 
                           <img src={photo} alt="userPhoto" width={20} height={20} style={{borderRadius:'10px'}} />
