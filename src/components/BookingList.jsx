5ff4cc70fdb134491e440d524d40d65b66c74534
Felix Owolabi <owolabifelix78@gmail.com>
2023-08-30 12:45:41 +0100

    airbnb

--- src/components/BookingList.jsx
+++ src/components/BookingList.jsx
@@ -58,8 +58,8 @@ const BookingList = () => {
                </div> 
             }
             {
-                !loading && bookings.length < 1 &&(
-                    (<div className='shadow-xl text-center mt-2 py-4 px-6 mx-auto max-w-xs rounded-lg font-semibold text-xl text-white'>
+                bookings && bookings.length < 1 &&(
+                    (<div className='text-center mt-2 py-4 px-6 mx-auto max-w-xs rounded-lg font-semibold text-xl text-white'>
                         No Bookings yet!.<br/><Link className='underline' to={'/'}>Make a booking here</Link></div>
                         )
                 )
