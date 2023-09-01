90c56878cfe36281b60af7a23bea98a3d9f92f3a
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-01 20:46:22 +0100

    airbnb

--- src/components/AddressLink.jsx
+++ src/components/AddressLink.jsx
@@ -29,8 +29,8 @@ const AddressLink = ({children}) => {
   return (
          <>
            {  isModal && (
-            <div className='bg-[whitesmoke] blur-1 shareBox w-[50%] h-[40%] min-w-[319px]
-              fixed top-[20%] left-[25%] bottom-[30%] right-[30%] z-10 border border-primary rounded-xl p-4'>
+            <div className='bg-[whitesmoke] blur-1 shareBox w-[50%] h-[40%] min-w-[319px] left-[20%]
+              fixed top-[20%] sm:left-[25%] bottom-[30%] sm:right-[30%] z-10 border border-primary rounded-xl p-4'>
                 <div>
                   <div className='flex justify-between'>
                   <h1 className='text-xl'>Share this Place:</h1>
