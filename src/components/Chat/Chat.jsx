381c7513eeebeadb865e1a08d352738974d43d66
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-02 21:06:26 +0100

    airbnb

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -38,8 +38,8 @@ const Chat = () => {
               animate={{y:0}} transition={{type:'spring',stiffness:170}} 
               className=" fixed z-10 shadow-2xl bottom-16 rounded-t-2xl overflow-hidden right-6 w-[90%] min-h-[35rem] bg-white">
                 <div className="flex gap-2 items-center justify-between px-6 bg-primary text-white py-2">
-                    <div className="hidden text-white cursor-pointer" style={{color:'white'}}>
-                        <img src="/images/menu-open.png" alt="icon" style={{color:"white"}} height={20} width={20} />
+                    <div className=" text-white cursor-pointer" style={{color:'white'}}>
+                        <img src="/images/menu-open.png" alt="icon" className="hidden" height={20} width={20} />
                     </div>
                     <div className="flex items-center gap-3">
                     <img className="rounded-full bg-white p-1" src="/images/agentIcon.png" alt="agentIcon" width={35} height={35} />
@@ -49,7 +49,7 @@ const Chat = () => {
                 </div>
                     <div className="flex pt-4 gap-4 relative ">
                         <div className="relative leftChat">
-                            <div className="absolute hidden cursor-pointer -top-2 right-4"><img src="/images/menu-close.png" alt="icon" height={17} width={17} /></div>
+                            <div className="absolute closeIcon hidden cursor-pointer -top-2 right-4"><img src="/images/menu-close.png" alt="icon" height={17} width={17} /></div>
                             <PotentialChats/>
                             {userChats?.length < 1 ? null : (
                                 <div className="flex flex-col">
