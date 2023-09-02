6fdabb05c8801a2a717f445eb42e3ab14c655fc7
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-02 20:48:27 +0100

    airbnb

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -38,9 +38,9 @@ const Chat = () => {
               animate={{y:0}} transition={{type:'spring',stiffness:170}} 
               className=" fixed z-10 shadow-2xl bottom-16 rounded-t-2xl overflow-hidden right-6 w-[90%] min-h-[35rem] bg-white">
                 <div className="flex gap-2 items-center justify-between px-6 bg-primary text-white py-2">
-                    <motion.div className="overflow-hidden" initial={{scale:0.9}} animate={{scale:1}} transition={{type:'spring',stiffness:130}}></motion.div>
+                    {/* <motion.div className="overflow-hidden" initial={{scale:0.9}} animate={{scale:1}} transition={{type:'spring',stiffness:130}}></motion.div> */}
                     <div>
-                        <img src="/images/menu-open.png" alt="icon" height={17} width={17} />
+                        <img src="/images/menu-open.png" alt="icon" height={20} width={20} />
                     </div>
                     <div className="flex items-center gap-3">
                     <img className="rounded-full bg-white p-1" src="/images/agentIcon.png" alt="agentIcon" width={35} height={35} />
