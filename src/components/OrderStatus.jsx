21f268733660f6d70b686829cfa1ab863624f5ae
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-02 12:31:09 +0100

    airbnb

--- src/components/OrderStatus.jsx
+++ src/components/OrderStatus.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { motion } from "framer-motion";
+import { AnimatePresence,motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { format } from 'date-fns';
