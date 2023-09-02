3aa543975ae2ecc353fb8c5a702fcde3e94a80d8
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-02 13:49:19 +0100

    airbnb

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -24,7 +24,7 @@ const ResetPassword = () => {
     return (  
             <>
               <div className="flex justify-center items-center min-h-[80vh]">
-              <motion.div initial={{opacity:0,y:50}} animate={{opacity:1,y:0}} className="border border-primary rounded-2xl shadow-xl p-8  w-[70%]">
+              <motion.div initial={{opacity:0,y:50}} animate={{opacity:1,y:0}} transition={{duration:1,type:'spring',stiffness:100}} className="border border-primary rounded-2xl shadow-xl p-8  w-[70%]">
                 <h1 className=" flex text-lg justify-center gap-2 items-center font-semibold"> <span>Forgot Password</span><svg fill="none" width={20} height={20} stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z"></path>
                 </svg></h1>
