0b42521dd2696a4d062015bf9d7b551c74381dfc
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-02 13:01:13 +0100

    airbnb

--- src/components/OrderStatus.jsx
+++ src/components/OrderStatus.jsx
@@ -28,10 +28,10 @@ const OrderStatus = () => {
            <>
              <div className=" orderReceipt w-[80%] relative shadow-2xl mx-auto px-8">
                 <div>
-                <span className="flex justify-center p-4"><svg width={80} height={80} fill="none" stroke="#FF385C" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <span className="flex justify-center p-4"><svg className=" w-[45px] h-[45px] sm:w-[80px] sm:h-[80px]" fill="none" stroke="#FF385C" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path  stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg></span>
-                <h1 className="py-2 border-b-2 text-center text-xl border-primary">Reservation Details</h1>
+                <h1 className="py-2 border-b-2 text-center text-lg sm:text-xl border-primary">Reservation Details</h1>
                 </div>
                 <AnimatePresence>
                     { isLoading && (
@@ -50,25 +50,25 @@ const OrderStatus = () => {
                       !isLoading && order && (
                               <>
                               <div className="">
-                              <img className="absolute opacity-10 max-w-[100%] -z-10 top-[37%] left-[37%]" height={300} width={300} src="/images/airbnb.png" alt="airbnb-logo" />
+                              <img className="absolute opacity-10 max-w-[100%] -z-10 top-[42%] left-[42%] sm:top-[37%] sm:left-[37%] h-[130px] w-[130px] sm:h-[300px] sm:w-[300px]" src="/images/airbnb.png" alt="airbnb-logo" />
 
                           <span className="flex my-1 py-1 w-44 mx-auto rounded-xl bg-green-500 text-white font-semibold items-center justify-center">Status:<span>{order.status}</span></span>
-                              <h1 className="text-xl pb-2 text-gray-900">Customer :</h1>
-                              <div className="flex flex-col text-gray-700 border-b-2 pb-2 ">
+                              <h1 className=" text-lg sm:text-xl pb-2 text-gray-900">Customer :</h1>
+                              <div className=" text-sm sm:text-base flex flex-col text-gray-700 border-b-2 pb-2 ">
                               <span className="flex justify-between">Full-Name: <span>{order.details[0].fullName}</span> </span>
                               <span className="flex justify-between">Country: <span>{order.country}</span></span>
                               <span className="flex justify-between">Phone-Number: <span>{order.details[0].mobile}</span></span>
                               <span className="flex justify-between">Email: <span>{order.email}</span></span>
                               </div>
-                              <h1 className="text-xl pb-2 pt-2 text-gray-900">Payments:</h1>
-                          <div className="flex flex-col text-gray-700 border-b-2 pb-2 ">
+                              <h1 className="text-lg sm:text-xl pb-2 pt-2 text-gray-900">Payments:</h1>
+                          <div className=" text-sm sm:text-base flex flex-col text-gray-700 border-b-2 pb-2 ">
                           <span className="flex justify-between">Payment-Intent: <span>{order.paymentIntentId}</span></span>
                           <span className="flex justify-between">Payment-Status: <span>{order.payment_status}</span></span>
                           <span className="flex justify-between">Amount Paid: <span>${order.details[0].price}</span></span>
                           <span className="flex justify-between">Payment-Time: <span>{format(new Date(order.paymentTime *1000),'dd MMMM, yyyy HH:mm:ss a')}</span></span>
                           </div>
-                            <h1 className="text-xl pb-2 pt-2 text-gray-900">Bookings:</h1>
-                            <div className="flex flex-col text-gray-700 ">
+                            <h1 className=" text-lg sm:text-xl pb-2 pt-2 text-gray-900">Bookings:</h1>
+                            <div className="text-sm sm:text-base flex flex-col text-gray-700 ">
                           <span className="flex justify-between">Booking Number:<span>{order.customerId}</span></span> 
                           <span className="flex justify-between">Booking Location: <span className="w-[200px] truncate sm:w-auto">{order.bookingPlace}</span> </span>
                           <span className="flex justify-between">Booking Address:<span>{order.bookingAddress}</span></span> 
@@ -88,7 +88,7 @@ const OrderStatus = () => {
                 </div>
              </div>
              { order && (
-                  <div className="mx-auto max-w-2xl bg-gray-200 p-8 rounded-lg m-6">
+                  <div className="text-sm sm:text-base mx-auto max-w-2xl bg-gray-200 p-8 rounded-lg m-6">
                       <h1>Hii,<span className='text-green-500 font-bold'>{order.details[0].fullName}</span>. Here's a confirmation detail of your successful transaction.Kindly ensure to keep this information safe as it is very important. Details about your host and explicit information about the instructions you need to adhere to as regards your reservation would be forwarded to you shortly.Enjoy!</h1>
                   </div>
              )
