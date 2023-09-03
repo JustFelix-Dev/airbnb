df193e38127349dacdc368ba73d3511c698336a8
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-03 04:27:31 +0100

    airbnb

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -66,7 +66,7 @@ const ProfilePage = ({user,setUser,setRedirected}) => {
 
             {  user && 
             <div className='w-full sm:w-[80%] p-4 shadow-2xl rounded-lg mx-auto'>
-  <div className="flex flex-col profilepage  sm:flex-row gap-10 p-4 border border-dashed border-primary rounded-xl">
+  <div className="flex profilepage gap-10 p-4 border border-dashed border-primary rounded-xl">
     <div className='sm:border-r border-primary '>
       <div className='flex justify-center p-4'>
         <img 
