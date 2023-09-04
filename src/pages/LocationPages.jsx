c497698aeb28b14f47ad892502c8a039159151aa
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-04 12:30:37 +0100

    airbnb

--- src/pages/LocationPages.jsx
+++ src/pages/LocationPages.jsx
@@ -114,7 +114,7 @@ const LocationPages = () => {
                                <PlacesImage place={place}/>
                             </div>
                             <div className='grow-0 shrink'>
-                                <h2 className='text-xl locationTitle'>{place.title}</h2>
+                                <h2 className='truncate text-xl locationTitle'>{place.title}</h2>
                                 <p className='text-sm mt-2 locationDescription h-[50px] overflow-auto'>{place.description}</p>
                             </div>
                             <div className='flex gap-2 relative cursor-pointer' >
