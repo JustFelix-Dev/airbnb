69ea05e74888fcb7662d90482a789c84dbc497a0
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-07 09:48:38 +0100

    airbnb

--- src/components/Chat/PotentialChats.jsx
+++ src/components/Chat/PotentialChats.jsx
@@ -7,18 +7,15 @@ const PotentialChats = () => {
     const {potentialChats,createChat,onlineUsers} = useContext(ChatContext);
   return (
        <>
-           
+           <div className='support-services'><img src="/images/support-services.png" alt="icon" height={30} width={30} /></div>
          <div className=" potential-support flex gap-1 mb-2 ml-2">
             {potentialChats.map((support,index)=>{
                 return (
-                  <>
-                    <div className='support-services'><img src="/images/support-services.png" alt="icon" height={30} width={30} /></div>
                 <div className=" cursor-pointer bg-primary text-white py-1 px-4 rounded-lg relative" key={index} onClick={()=>createChat(user._id,support._id)}>
                     {support.name}
                     <span className={ onlineUsers?.some((user)=> user?.userId === support._id) ? `inline-block h-[8px] w-[8px] rounded-full bg-green-400 absolute -top-[3px] 
                     -right-[3px] z-2` : ""}></span>
                  </div>
-                 </>
                       )
             })}
          </div>
