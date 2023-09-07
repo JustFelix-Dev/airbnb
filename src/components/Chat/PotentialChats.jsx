c0d7f76f10082b62ed38c522889858e40351dedf
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-07 10:17:12 +0100

    airbnb

--- src/components/Chat/PotentialChats.jsx
+++ src/components/Chat/PotentialChats.jsx
@@ -1,14 +1,23 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ChatContext } from '../../ContextHook/chatContext';
 import { userContext } from '../../ContextHook/userContext';
+import { AnimatePresence,motion } from 'framer-motion';
 
 const PotentialChats = () => {
     const {user} = useContext(userContext);
+    const [ showPChats,setShowPChats] = useState(true);
     const {potentialChats,createChat,onlineUsers} = useContext(ChatContext);
+
+
+    const showPotentialChats = ()=>{
+         setShowPChats(!showPChats)
+    }
   return (
        <>
-           <div className='support-services'><img src="/images/support-services.png" alt="icon" height={30} width={30} /></div>
-         <div className=" potential-support flex gap-1 mb-2 ml-2">
+           <div onClick={showPotentialChats} className='support-services'><img src="/images/support-services.png" alt="icon" height={30} width={30} /></div>
+          <AnimatePresence>
+         { 
+         showPChats &&  <motion.div exit={{opacity:0,scale:0.9}} initial={{opacity:0,scale:0.9}} animate={{opacity:1,scale:1}} className=" potential-support flex gap-1 mb-2 ml-2">
             {potentialChats.map((support,index)=>{
                 return (
                 <div className=" cursor-pointer bg-primary text-white py-1 px-4 rounded-lg relative" key={index} onClick={()=>createChat(user._id,support._id)}>
@@ -18,7 +27,9 @@ const PotentialChats = () => {
                  </div>
                       )
             })}
-         </div>
+         </motion.div>
+         }
+         </AnimatePresence>
        </>
   )
 }
