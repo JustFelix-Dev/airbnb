bb1b7999b429fc3b07e224b2d863e3c59a2367fb
Felix Owolabi <owolabifelix78@gmail.com>
2023-09-09 10:26:34 +0100

    airbnb

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -75,7 +75,7 @@ const IndexPage = () => {
                 </div>
               </Link>
             ))}
-            <div className="border border-primary flex items-center justify-center p-2">
+            <div className="border w-full border-primary flex items-center justify-center p-2">
             <div className="scene">
               <div className="cube">
                 <span className="side top">Thank You😊</span>
